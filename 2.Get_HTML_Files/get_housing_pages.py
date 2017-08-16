933ab300967914777b00d6cc1db906b1a5f896a1
Keyan Vakil <keyanv@bu.edu>
2017-08-16 13:16:10 -0400

    Fix bug that prevent cities with '.' characters from being read fully into the url; Temporarily add condition to download affected pages.

--- 2.Get_HTML_Files/get_housing_pages.py
+++ 2.Get_HTML_Files/get_housing_pages.py
@@ -4,26 +4,31 @@ import time
 import random
 import urllib
 
-
+# WILL FAIL FOR ST. LOUIS, FIXME
 def main():
     HOUSING_FOLDER = 'Housing/'
     SALARY_FOLDER = 'All_Salaries/'
 
+    parsed_housing = glob.glob(os.path.join(HOUSING_FOLDER))
+
     for file_name in glob.glob(os.path.join(SALARY_FOLDER, '*.html')):
         city_name = file_name_to_city_name(file_name)
         print city_name
+        if '.' in city_name:
+            link = 'https://www.zillow.com/'+city_name+'/home-values/'
+            urllib.URLopener.version = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
 
-        link = 'https://www.zillow.com/'+city_name+'/home-values/'
-        urllib.URLopener.version = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
+            urllib.urlretrieve(link, HOUSING_FOLDER + city_name + '.html')
 
-        urllib.urlretrieve(link, HOUSING_FOLDER + city_name + '.html')
-
-        # avoid spamming the website
-        time.sleep(30+random.random()*15)
+            # avoid spamming the website
+            time.sleep(30+random.random()*15)
 
 
 def file_name_to_city_name(file_name):
-    return file_name.split('/')[1].split('.')[0]
+    name_html = file_name.split('/')[1]
+    # removes the .html extension
+    name = name_html[:-5]
+    return name
 
 
 if __name__ == '__main__':
