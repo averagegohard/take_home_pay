b3436f8ff4428d1ff18bcad82a0784e3e6eff516
Keyan Vakil <keyanv@bu.edu>
2017-08-15 23:21:41 -0400

    Convert tabs to spaces (not that it matters either way)

--- 2.Get_HTML_Files/get_experience_links.py
+++ 2.Get_HTML_Files/get_experience_links.py
@@ -7,31 +7,31 @@ site = 'http://www.payscale.com'
 
 # input should probably be more extensible and merged with store_HTML.py
 def main(level_folder='Entry-Level/'):
-	global site
-	BASE_FOLDER_NAME = 'All_Salaries/'
+    global site
+    BASE_FOLDER_NAME = 'All_Salaries/'
 
-	# copied from SO, used to access all html files in the directory
-	for filename in glob.glob(os.path.join(BASE_FOLDER_NAME, '*.html')):
-	    print filename
-		soup = BeautifulSoup(open(filename))
+    # copied from SO, used to access all html files in the directory
+    for filename in glob.glob(os.path.join(BASE_FOLDER_NAME, '*.html')):
+        print filename
+        soup = BeautifulSoup(open(filename))
 
 
-		for a in soup.findall('a', href=True):
-			# if the name is in the link, we know it is what
-			# we are looking for
-			if level_folder in a['href']:
+        for a in soup.findall('a', href=True):
+            # if the name is in the link, we know it is what
+            # we are looking for
+            if level_folder in a['href']:
 
-				# this should probably be a function in its own module
-				# instead of copy/pasting
-				# *there probably should be a better way to store the info
-				# access and store the html file
-				urllib.urlretrieve(site+a['href'], level_folder+'/'+filename)
+                # this should probably be a function in its own module
+                # instead of copy/pasting
+                # *there probably should be a better way to store the info
+                # access and store the html file
+                urllib.urlretrieve(site+a['href'], level_folder+'/'+filename)
 
-				# avoid spamming the website
-				time.sleep(30+random.random()*15)
+                # avoid spamming the website
+                time.sleep(30+random.random()*15)
 
-				break
+                break
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- 2.Get_HTML_Files/store_HTML.py
+++ 2.Get_HTML_Files/store_HTML.py
@@ -9,32 +9,32 @@ state_codes = us.states.mapping('name', 'abbr')
 
 # may need to rewrite to be extensible
 def main():
-	'''
-	access and store the html files to minimize the number of requests
-	future improvements should allow the program to pick up where it
-	left off, similar to get_city_links.py.
-	
-	future improvements could also just extract the values required
-	directly from the HTML files in case space becomes an issue
-	'''
-	links = '../1.Get_Links/processed_links/city_links.csv'
+    '''
+    access and store the html files to minimize the number of requests
+    future improvements should allow the program to pick up where it
+    left off, similar to get_city_links.py.
+    
+    future improvements could also just extract the values required
+    directly from the HTML files in case space becomes an issue
+    '''
+    links = '../1.Get_Links/processed_links/city_links.csv'
 
-	for line in open(links, 'r'):
-		# could be changed into an object, but ehhh
-		city_info = line.strip().split(',')
-		link = city_info[-1]
+    for line in open(links, 'r'):
+        # could be changed into an object, but ehhh
+        city_info = line.strip().split(',')
+        link = city_info[-1]
 
-		FOLDER_NAME = 'All_Salaries/'
+        FOLDER_NAME = 'All_Salaries/'
 
-		file_name = FOLDER_NAME + extract_formatted_name(city_info)+'.html'
-		print file_name
+        file_name = FOLDER_NAME + extract_formatted_name(city_info)+'.html'
+        print file_name
 
-		# *there probably should be a better way to store the info
-		# access and store the html file
-		urllib.urlretrieve(link, file_name)
+        # *there probably should be a better way to store the info
+        # access and store the html file
+        urllib.urlretrieve(link, file_name)
 
-		# avoid spamming the website
-		time.sleep(30+random.random()*15)
+        # avoid spamming the website
+        time.sleep(30+random.random()*15)
 
 
 # technically this should be placed in its own module instead of
@@ -46,4 +46,4 @@ def extract_formatted_name(values):
 
 
 if __name__ == '__main__':
-	main()
+    main()
