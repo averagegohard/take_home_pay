3128d481a33519664728b0d50481aa65a75aa10f
keyan vakil <youraveragegohard@gmail.com>
2017-08-18 08:47:18 -0400

    Major bug fixes; Add more detailed print statements

--- 2.Get_HTML_Files/get_housing_pages.py
+++ 2.Get_HTML_Files/get_housing_pages.py
@@ -12,22 +12,27 @@ def main():
     global HOUSING_FOLDER
     global SALARY_FOLDER
 
-    for file_name in glob.glob(os.path.join(SALARY_FOLDER, '*.html')):
+    for file_name in get_files_in_folder(SALARY_FOLDER):
         download_page(file_name)
 
+def get_files_in_folder(folder, filetype='.html'):
+    return glob.glob(os.path.join(folder, '*' + filetype))
 
 def download_page(file_name):
-    time.sleep(sleep_time)
     if not exists(file_name):
+        print file_name_to_city_name(file_name), 'page has not been downloaded'
         get_page(file_name)
     elif is_captcha(file_name):
+        print file_name, 'is a captcha page'
         get_page(file_name)
 
 def exists(file_name):
     global HOUSING_FOLDER
-    parsed_housing = glob.glob(os.path.join(HOUSING_FOLDER))
-    # checks if a file already exists
-    return file_name in parsed_housing
+    parsed_housing = get_files_in_folder(HOUSING_FOLDER)
+
+    tmp_name = HOUSING_FOLDER + file_name_to_city_name(file_name) + '.html'
+
+    return tmp_name in parsed_housing
 
 
 def get_page(file_name, sleep_time = 30):
@@ -37,7 +42,7 @@ def get_page(file_name, sleep_time = 30):
     global HOUSING_FOLDER
     # get the pages
     city_name = file_name_to_city_name(file_name)
-    print city_name
+    print 'Downloading', city_name
 
     link = 'https://www.zillow.com/'+city_name+'/home-values/'
     urllib.URLopener.version = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
@@ -48,7 +53,7 @@ def get_page(file_name, sleep_time = 30):
     if is_captcha(file_name):
         # if we receive a captcha page, increase the sleep time by a random factor
         # so eventually the captcha will be resolved
-        get_page(file_name, sleep_time*(1+random.random())
+        get_page(file_name, sleep_time*(1+random.random()))
 
 
 # will need more robust check in the future
