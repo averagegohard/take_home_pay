223713d77b9ac4b7206e9486fe03571a309d724d
keyan vakil <youraveragegohard@gmail.com>
2017-08-18 09:27:53 -0400

    Create function to map files in SALARY_FOLDER to HOUSING_FOLDER; Refactor

--- 2.Get_HTML_Files/zillow_housing_data.py
+++ 2.Get_HTML_Files/zillow_housing_data.py
@@ -8,6 +8,7 @@ import urllib
 HOUSING_FOLDER = 'Housing/'
 SALARY_FOLDER = 'All_Salaries/'
 
+
 def main():
     global HOUSING_FOLDER
     global SALARY_FOLDER
@@ -15,24 +16,41 @@ def main():
     for file_name in get_files_in_folder(SALARY_FOLDER):
         download_page(file_name)
 
+
+def city_name(file_name):
+    file = file_name.split('/')[1]
+    # removes the .html extension
+    name = file[:-5]
+    return name
+
+
 def get_files_in_folder(folder, filetype='.html'):
     return glob.glob(os.path.join(folder, '*' + filetype))
 
+
 def download_page(file_name):
-    if not exists(file_name):
-        print file_name_to_city_name(file_name), 'page has not been downloaded'
-        get_page(file_name)
-    elif is_captcha(file_name):
-        print file_name, 'is a captcha page'
-        get_page(file_name)
+    name = city_name(file_name)
+    housing_file = create_housing_path(file_name)
+    if not exists(housing_file):
+        print name, 'page has not been downloaded'
+        get_page(housing_file)
+
+    elif is_captcha(housing_file):
+        print name, 'is a captcha page'
+        get_page(housing_file)
+
+    else:
+        print name, 'is already stored correctly'
+
+
+def create_housing_path(file_name):
+    global HOUSING_FOLDER
+    tmp_name = HOUSING_FOLDER + city_name(file_name) + '.html'
+    return tmp_name
+
 
 def exists(file_name):
-    global HOUSING_FOLDER
-    parsed_housing = get_files_in_folder(HOUSING_FOLDER)
-
-    tmp_name = HOUSING_FOLDER + file_name_to_city_name(file_name) + '.html'
-
-    return tmp_name in parsed_housing
+    return file_name in get_files_in_folder(HOUSING_FOLDER)
 
 
 def get_page(file_name, sleep_time = 30):
@@ -41,14 +59,14 @@ def get_page(file_name, sleep_time = 30):
     # functionality for acquiring the page
     global HOUSING_FOLDER
     # get the pages
-    city_name = file_name_to_city_name(file_name)
-    print 'Downloading', city_name
+    name = city_name(file_name)
+    print 'Downloading', name
 
-    link = 'https://www.zillow.com/'+city_name+'/home-values/'
+    link = 'https://www.zillow.com/'+name+'/home-values/'
     urllib.URLopener.version = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
 
     time.sleep(sleep_time)
-    file_name, _ = urllib.urlretrieve(link, HOUSING_FOLDER + city_name + '.html')
+    file_name, _ = urllib.urlretrieve(link, HOUSING_FOLDER + name + '.html')
     
     if is_captcha(file_name):
         # if we receive a captcha page, increase the sleep time by a random factor
@@ -60,13 +78,6 @@ def get_page(file_name, sleep_time = 30):
 def is_captcha(file_name):
     with open(file_name) as f:
         return 'captcha' in f.read()
-
-
-def file_name_to_city_name(file_name):
-    name_html = file_name.split('/')[1]
-    # removes the .html extension
-    name = name_html[:-5]
-    return name
 
 
 if __name__ == '__main__':
