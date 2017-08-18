75679186f57d799bd272d4ddf511e533681589ec
keyan vakil <youraveragegohard@gmail.com>
2017-08-18 08:34:52 -0400

    Add functionality to test and fix captcha page downloads

--- 2.Get_HTML_Files/get_housing_pages.py
+++ 2.Get_HTML_Files/get_housing_pages.py
@@ -4,24 +4,57 @@ import time
 import random
 import urllib
 
-# WILL FAIL FOR ST. LOUIS, FIXME
-def main():
-    HOUSING_FOLDER = 'Housing/'
-    SALARY_FOLDER = 'All_Salaries/'
+# TODO find way to avoid using ugly global variables
+HOUSING_FOLDER = 'Housing/'
+SALARY_FOLDER = 'All_Salaries/'
 
-    parsed_housing = glob.glob(os.path.join(HOUSING_FOLDER))
+def main():
+    global HOUSING_FOLDER
+    global SALARY_FOLDER
 
     for file_name in glob.glob(os.path.join(SALARY_FOLDER, '*.html')):
-        city_name = file_name_to_city_name(file_name)
-        print city_name
-        if '.' in city_name:
-            link = 'https://www.zillow.com/'+city_name+'/home-values/'
-            urllib.URLopener.version = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
+        download_page(file_name)
 
-            urllib.urlretrieve(link, HOUSING_FOLDER + city_name + '.html')
 
-            # avoid spamming the website
-            time.sleep(30+random.random()*15)
+def download_page(file_name):
+    time.sleep(sleep_time)
+    if not exists(file_name):
+        get_page(file_name)
+    elif is_captcha(file_name):
+        get_page(file_name)
+
+def exists(file_name):
+    global HOUSING_FOLDER
+    parsed_housing = glob.glob(os.path.join(HOUSING_FOLDER))
+    # checks if a file already exists
+    return file_name in parsed_housing
+
+
+def get_page(file_name, sleep_time = 30):
+    sleep_time += random.random()*15
+
+    # functionality for acquiring the page
+    global HOUSING_FOLDER
+    # get the pages
+    city_name = file_name_to_city_name(file_name)
+    print city_name
+
+    link = 'https://www.zillow.com/'+city_name+'/home-values/'
+    urllib.URLopener.version = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
+
+    time.sleep(sleep_time)
+    file_name, _ = urllib.urlretrieve(link, HOUSING_FOLDER + city_name + '.html')
+    
+    if is_captcha(file_name):
+        # if we receive a captcha page, increase the sleep time by a random factor
+        # so eventually the captcha will be resolved
+        get_page(file_name, sleep_time*(1+random.random())
+
+
+# will need more robust check in the future
+def is_captcha(file_name):
+    with open(file_name) as f:
+        return 'captcha' in f.read()
 
 
 def file_name_to_city_name(file_name):
